acef9d2f712481a31dd6813ff859a26fb38b4c5d
mr-jaydip-2283 <jaydipdandwate2283@gmail.com>
2025-02-18 14:55:20 +0530

    success

--- ems/settings.py
+++ ems/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,8 @@ STATICFILES_DIRS = (
     # location of your application, should not be public web accessible 
     './static',
 )
+
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
